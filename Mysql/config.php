37e03d504a659f56144aeb19677a127f16caa2d2
Chaiyut <95678221+ChaiyutK@users.noreply.github.com>
2022-01-06 20:06:32 +0700

    comment in config.php

--- Mysql/config.php
+++ Mysql/config.php
@@ -1,6 +1,6 @@
 <?php
-$dbhost = "localhost";
-$dbuser = "root";
+$dbhost = "localhost"; //this is localhost you can change
+$dbuser = "root"; //this is example dbuser please don't use root to be user.
 $dbpass = "";
 $dbname = "goodsoundeffect";
 ?>
